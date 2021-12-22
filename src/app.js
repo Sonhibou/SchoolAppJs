aa4ebf5685ca3494a3f9e4cb8e5bb1c0c4cf6da2
Sonhibou_CN <sonhibousimplon@gmail.com>
2021-12-22 18:11:56 +0000

    Feat : Insertion du formulaire d'insertion Apprenant

---
+++ src/app.js
@@ -0,0 +1,10 @@
+const apprenant = {
+    nom: "Sene",
+    prenom : "Serigne Sonhibou",
+    niveau : "Bien"
+}
+console.log('Ajouter')
+btnAjouter = document.querySelector(".btnAjouter");
+btnAjouter,addEventListener("click", (event)=>{
+    alert("Bonjour")
+})
